ec5529ab42cba31c2e0ababffdbee85fdbff3bec
dinara994 <di_94@mail.ru>
2021-07-28 18:36:51 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// alert('Привет! это сложение чисел,') //всплывающее окно
+// const a = prompt('Выведиьте число a')
+// const b = prompt('Выведиьте число b')
+// alert(Number(a) + Number(b)) //Выводит из СТРОКИ Число и СКЛАДЫВАЕТ
+
+// let answer = confirm('Вам есть 18?') // Всплывающем окне задает окно и выдает TRUE или False
+// alert(answer)
+
+// Видео 00:17 мин
+// console.log(window) // Глобальтный обьект нашей страницы еще 00:38. 00:49
+
+// console.dir(document.children[0].children[1].children[0].textContent = 'Hello dear!')
+//
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
